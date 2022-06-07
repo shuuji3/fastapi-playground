f5df3ff86c70014720c9aee93d02edf451a0e5a2
TAKAHASHI Shuuji <shuuji3@gmail.com>
2022-06-07 22:48:40 +0900

    feat: add /fetch/{url} endpoint

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
 
@@ -11,3 +12,9 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get('/fetch/{url}')
+async def fetch(url: str):
+    response = requests.get(f'https://{url}')
+    return {'response': response.content}
